697df6912b954663e1da147a9480dbb5a2b403af
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-05-15 11:07:44 +0530

    initial setup (#1)
    * Create docker-compose-caliper.yaml
    
    * adding caliper setup files
    
    * Create config.yaml
    
    * Create test.js
    
    * Create network-config.yaml
    
    * Update network-config.yaml
    
    * Update network-config.yaml
    
    * Update test.js
    
    * Update README.md
    
    authored-by: Salman Dabbakuti <Salmandabbakuti>

---
+++ caliper-benchmarks/benchmarks/scenario/simple/basic-network/test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports.info = 'Adding Owner';
+let txnPerBatch;
+let bc, contx;
+module.exports.init = function (blockchain, context, args) {
+
+    if (!args.hasOwnProperty('txnPerBatch')) {
+        args.txnPerBatch = 1;
+    }
+    txnPerBatch = args.txnPerBatch;
+    bc = blockchain;
+    contx = context;
+
+    return Promise.resolve();
+};
+
+function generateWorkload() {
+    let workload = [];
+    for (let i = 0; i < txnPerBatch; i++) {
+
+        workload.push({
+            chaincodeFunction: 'registerOwner',
+            chaincodeArguments: ["salmandev", "12345", "dev"],
+        });
+    }
+    return workload;
+}
+
+module.exports.run = function () {
+    try {
+        let args = generateWorkload();
+        return bc.invokeSmartContract(contx, 'mycc', '1.0', args);
+    } catch (err) {
+        console.log(err)
+    }
+
+};
+
+module.exports.end = function () {
+    return Promise.resolve();
+};
